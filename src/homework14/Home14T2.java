55fad4cb5c8d58602d91a32de40521d1ef6a8728
Hanna <hannaermolenko1014@gmail.com>
2024-02-04 20:10:19 +0100

    task 3 did

---
+++ src/homework14/Home14T2.java
@@ -0,0 +1,13 @@
+package homework14;
+
+public class Home14T2 {
+   // public static void main(String[] args) {
+
+    }
+
+  //  public static String getPartString(String inputString, int startIndex, int endIndex){
+  //      for (int i = 0; i < inputString.length(); i++){
+  //          char str = string.charAt(i);
+
+
+
